1b54bd812a98b54cc01fdb39e006d0c3ca3072e7
shivl <shivlall0009@gmail.com>
2025-01-27 07:20:56 -0500

    refactor the StudentRestController for clean coding

--- 04-spring-boot-restController/restController/src/main/java/com/example/restAPI/restController/rest/StudentRestController.java
+++ 04-spring-boot-restController/restController/src/main/java/com/example/restAPI/restController/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.example.restAPI.restController.rest;
 
 import com.example.restAPI.restController.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,25 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define a endpoint for students and return a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<Student>();
+    private List<Student> theStudents;
+
+    // define  a postconstruct to load the student data only once
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<Student>();
         theStudents.add(new Student("Max", "load"));
         theStudents.add(new Student("Jack", "willsen"));
         theStudents.add(new Student("John", "nest"));
 
+    }
+
+    // define an endpoint for students and return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
         // jackson will convert the code to json by automatically
         return theStudents;
     }
+
 }
