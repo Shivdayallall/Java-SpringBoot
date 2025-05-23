31f2653725e3de662b53dbbe2bb51271bb92d991
shivl <shivlall0009@gmail.com>
2025-05-23 17:52:42 -0400

    Add crud to employee app

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleaftDemo/ThymeleaftDemoApplication.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleaftDemo/ThymeleaftDemoApplication.java
@@ -1,13 +1,13 @@
-package com.luv2code.springboot.cruddemo;
+package com.luv2code.springboot.thymeleaftDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CruddemoApplication {
+public class ThymeleaftDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
+		SpringApplication.run(ThymeleaftDemoApplication.class, args);
 	}
 
 }
